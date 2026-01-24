42e083586bc02984669a86c640ff3e8e3977fa33
1SamXD <johnson.samuel2005@gmail.com>
2026-01-24 03:55:28 +0000

    added construction paragraph, skills selection, 5 skills to learn

--- src/main.cob
+++ src/main.cob
@@ -21,6 +21,7 @@
        WORKING-STORAGE SECTION.
            01 MAX-USERS PIC 9 VALUE 5.
            01 WELCOME-CHOICE PIC 9.
+           01 USER-CHOICE PIC 9.
            01 IN-USER PIC X(15).
            01 IN-PASS PIC X(15).
            01  LOGIN-SUCCESS PIC 9 VALUE 0.
@@ -32,6 +33,7 @@
        STOP RUN.
 
        MAIN-MENU.
+           MOVE 0 TO WELCOME-CHOICE.
            DISPLAY "============================".
            DISPLAY "Welcome to InCollege!"
            DISPLAY "============================".
@@ -41,12 +43,14 @@
            DISPLAY "3. Exit".
            ACCEPT WELCOME-CHOICE.
            EVALUATE WELCOME-CHOICE
-               WHEN 1
+               WHEN 2
                    PERFORM LOGIN
+               WHEN 3
+                   STOP RUN
            END-EVALUATE.
 
        LOGIN.
-           DISPLAY "============================".
+           DISPLAY     "============================".
            DISPLAY "LOGIN PAGE".
            DISPLAY "Enter Username:".
            ACCEPT IN-USER.
@@ -70,8 +74,10 @@
 
            IF LOGIN-SUCCESS = 1
                DISPLAY "Login Successful!"
+               PERFORM USER-OPTIONS
            ELSE
-               DISPLAY "Login Failed! Invalid username or password"
+               DISPLAY "Incorrect username/password, please try again."
+               PERFORM LOGIN
            END-IF.
 
        CHECK-CREDENTIALS.
@@ -81,3 +87,74 @@
                    MOVE 1 TO FILE-EOF
                END-IF
            END-IF.
+
+       USER-OPTIONS.
+           MOVE 0 TO USER-CHOICE.
+           DISPLAY "============================".
+           DISPLAY "1. Job Search".
+           DISPLAY "2. Find Someone".
+           DISPLAY "3. Learn Skill".
+           DISPLAY "4. Exit".
+
+           ACCEPT USER-CHOICE.
+           EVALUATE USER-CHOICE
+               WHEN 1
+                   PERFORM JOB-SEARCH
+               WHEN 2
+                   PERFORM FIND-SOMEONE
+               WHEN 3
+                   PERFORM LEARN-SKILL
+               WHEN 4
+                   STOP RUN
+           END-EVALUATE.
+
+       JOB-SEARCH.
+           PERFORM CONSTRUCTION.
+       FIND-SOMEONE.
+           PERFORM CONSTRUCTION.
+
+       LEARN-SKILL.
+           MOVE 0 TO USER-CHOICE.
+           DISPLAY "============================".
+           DISPLAY "Choose a new skill to learn:".
+           DISPLAY "1. Programming in COBOL".
+           DISPLAY "2. Programming in Python".
+           DISPLAY "3. Programming in C++".
+           DISPLAY "4. CAD Modeling".
+           DISPLAY "5. PCB Design".
+           DISPLAY "6. Back to Main Menu".
+
+           ACCEPT USER-CHOICE.
+           EVALUATE USER-CHOICE
+               WHEN 1
+                   PERFORM LEARN-COBOL
+               WHEN 2
+                   PERFORM LEARN-PYTHON
+               WHEN 3
+                   PERFORM LEARN-CPP
+               WHEN 4
+                   PERFORM LEARN-CAD
+               WHEN 5
+                   PERFORM LEARN-PCB
+               WHEN 6
+                   PERFORM USER-OPTIONS
+           END-EVALUATE.
+
+       LEARN-COBOL.
+           PERFORM CONSTRUCTION.
+       LEARN-PYTHON.
+           PERFORM CONSTRUCTION.
+       LEARN-CPP.
+           PERFORM CONSTRUCTION.
+       LEARN-CAD.
+           PERFORM CONSTRUCTION.
+       LEARN-PCB.
+           PERFORM CONSTRUCTION.
+
+       CONSTRUCTION.
+           MOVE 0 TO USER-CHOICE.
+           DISPLAY "============================".
+           DISPLAY "This feature is under construction. Please check back later!".
+           DISPLAY "1. Back to Main Menu".
+           ACCEPT USER-CHOICE.
+           PERFORM USER-OPTIONS.
